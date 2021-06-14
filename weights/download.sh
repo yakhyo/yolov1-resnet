214f19f0d5f4451d9dd36fba588656914e762fb9
yakhyo <yakhyo9696@gmail.com>
2021-06-14 17:28:16 +0900

    Update links

--- weights/download.sh
+++ weights/download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd ./weights
-wget -O yolo.pth https://www.dropbox.com/s/6morcod2n7sac2q/yolo.pth?dl=0
-wget -O best.pth https://www.dropbox.com/s/wbxqloaj5mzsoio/best.pth?dl=0
+wget -O yolo.pth https://www.dropbox.com/s/nt5mddry9kuhyte/yolo.pth?dl=0
+wget -O best.pth https://www.dropbox.com/s/a9nxmt6fcmgnkaj/best.pth?dl=0
